4519b37f83a12e0d4f9713deb98972a3bde3ef2b
etogood <ilya@shaura.ru>
2022-03-05 19:01:25 +0300

    glfw, glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main(){
-	return 0;
+// ReSharper disable once IdentifierTypo
+void glfw_window_size_callback(GLFWwindow* p_window, int width, int height)
+{
+	
+}
+
+void main(){
+
+	if (!glfwInit())
+	{
+		std::cerr << "Can't initialize glfw";
+		return;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 }
